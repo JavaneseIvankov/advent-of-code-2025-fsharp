43cf4be26afeb9cfbabf00523ec3ea2b019a9375
arundaya <runsworkspace@gmail.com>
2025-12-05 14:49:08 +0700

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {self, ...}:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = {pkgs, ...}: {
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              dotnetCorePackages.sdk_9_0-bin
+              fsharp
+            ];
+
+            DOTNET_BIN = "${pkgs.dotnetCorePackages.sdk_9_0-bin}/bin/dotnet";
+          };
+        };
+      };
+    };
+}
